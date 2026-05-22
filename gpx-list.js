21feaa07e77fd9b6031f98a9f19aa9a6e95c4623
ShibadogRueiiiii <tpc339228@gmail.com>
2026-05-22 22:33:09 +0800

    Add files via upload

---
+++ gpx-list.js
@@ -0,0 +1,23 @@
+const GPX_FILES=[
+
+{
+file:"gpx/印尼100.gpx",
+color:"#00c853"
+},
+
+{
+file:"gpx/印尼101.gpx",
+color:"#ff1744"
+},
+
+{
+file:"gpx/日本77.gpx",
+color:"#2979ff"
+},
+
+{
+file:"gpx/泰國50.gpx",
+color:"#ff9100"
+}
+
+]
